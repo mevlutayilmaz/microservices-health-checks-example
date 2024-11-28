6efab32f957fbce28028c8d244ce354accc4f3e0
Mevlüt Akif Yılmaz <yilmaz.akif2525@gmail.com>
2024-11-28 19:36:27 +0300

    service health checks - monitoring

--- Monitoring.Service/Program.cs
+++ Monitoring.Service/Program.cs
@@ -5,8 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddHealthChecksUI(
     settings =>
     {
-        settings.AddHealthCheckEndpoint("Service A", "https://localhost:7214/health")
-                .AddHealthCheckEndpoint("Service B", "https://localhost:7018/health")
+        settings.AddHealthCheckEndpoint("Service A", "https://localhost:7038/health")
+                .AddHealthCheckEndpoint("Service B", "https://localhost:7176/health")
                 .SetEvaluationTimeInSeconds(3)
                 .SetApiMaxActiveRequests(3);
 
@@ -15,7 +15,7 @@ builder.Services.AddHealthChecksUI(
         settings.ConfigureWebhooksEndpointHttpclient((serviceProvider, httpClient) =>
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "...."));
     }
-).AddSqlServerStorage("Server=localhost, 1433;Database=HealthCheckUIDB;User ID=SA;Password=1q2w3e4r+!;TrustServerCertificate=True");
+).AddSqlServerStorage("Server=.;Database=HealthCheckUIDB;Integrated Security=True;TrustServerCertificate=True;");
 
 var app = builder.Build();
 
